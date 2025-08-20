fffd8ca6fe12224f491ca960262300ce40f7623a
Leonardo Pasa <92491691+LeoPasa@users.noreply.github.com>
2025-08-20 21:15:56 +0000

    Q2 conclúido

--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -4,12 +4,17 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LogSimples implements Iterable<String>{
+    private static LogSimples instance;
     private List<String> mensagens;
 
-    public LogSimples(){
+    private LogSimples(){
         mensagens = new LinkedList<>();
     }
-
+    static public LogSimples getInstance(){
+        if(instance==null) instance = new LogSimples();
+        return (instance);
+    }
+    
     public void log(String m){
         String logM = LocalDate.now().toString() + " : " + m;
         mensagens.add(logM);
